b0b5f02121c73abc4f3f72220cfd638086e485ef
rachel-osborne <60946778+rachel-osborne@users.noreply.github.com>
2020-03-11 21:12:07 -0400

    Add files via upload

--- splitter.py
+++ splitter.py
@@ -1,38 +1,88 @@
-
 import misc_tools
 import random
 
 
-def create_routing(env, first_step='op1'):
+def create_routing(env, first_step='move15'):
     tasks = {
+
+        'move15': {
+            'location': env['section_A_kanban'],
+            # is this the right location to have the move take place from?
+            'worker': env['production_control'],
+            'manned': True,
+            'setup_time': 0,
+            'run_time': 1,
+            'teardown_time': 0,
+            'transit_time': 0,
+            'route_to': 'op15'
+        },
+
         'op15': {
             'location': env['assembly_bench'],
             'worker': env['assembler'],
-            'manned': manned,
+            'manned': True,
             'setup_time': 1,
             'run_time': 5,
             'teardown_time': 1,
             'transit_time': 1
+            'route_to': 'move16'
+        },
+
+        'move16': {
+            'location': env['assembler'],
+            # is this the right location to have the move take place from?
+            'worker': env['production_control'],
+            'manned': True,
+            'setup_time': 0,
+            'run_time': 1,
+            'teardown_time': 0,
+            'transit_time': 0,
             'route_to': 'op16'
         },
 
         'op16': misc_tools.make_quality_step(
             env=env,
             run_time=2,
-            route_to='op17',
+            route_to='move17',
             transit_time=1
         ),
+
+        'move17': {
+            'location': env['quality_bench'],
+            # is this the right location to have the move take place from?
+            'worker': env['production_control'],
+            'manned': True,
+            'setup_time': 0,
+            'run_time': 1,
+            'teardown_time': 0,
+            'transit_time': 0,
+            'route_to': 'op17'
+        },
+
         'op17': {
-            'location': env['machine'],
+            'location': env['VSWR_CTI'],
             'worker': env['technician'],
             'manned': True,
-            'setup_time': 1,
+            'setup_time': 10,
             'run_time': 2,
-            'teardown_time': 1,
+            'teardown_time': 10,
             'transit_time': 1,
-            'yeild': 1.00,
-            'route_to_pass': env['multiplexer_attempt_kanban'],
+            'yield': 94.23,
+            'route_to_pass': env['splitter_kanban'],
+            'route_to_fail': 'op17_debug'
         }
+
+        'op17_debug': {
+            'location': env['VSWR_CTI'],
+            'worker': env['technician'],
+            'manned': True,
+            'setup_time': 0,
+            'run_time': random.uniform(60,200),
+            'teardown_time': 0,
+            'transit_time': 0,
+            'route_to_': env['splitter_kanban'],
+        },
+
     }
 
     return misc_tools.make_steps(first_step=first_step, tasks=tasks)
@@ -42,4 +92,3 @@ def create_kanban_attrs(env):
     return misc_tools.make_kanban_attrs(order_gen=env['gener.splitter_attempt'],
                                         order_point=2, order_qty=5,
                                         init_qty=5, warmup_time=0)
-
